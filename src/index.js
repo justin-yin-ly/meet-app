b3268c321bda71b21bd002256d47bf23ff91df76
jl2032 <55512607+jl2032@users.noreply.github.com>
2024-03-27 16:49:25 -0400

    Attempted reinstall of Atatus as the test integration message wasn't appearing on the Atatus dashboard.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 import * as atatus from 'atatus-spa';
-atatus.config('8da9122784af496a949e88cb1e3163a1').install();
+atatus.config('8abeaf91783241dda9ecbece7384afb6').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -24,4 +24,4 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-atatus.notify(new Error('Test Atatus Setup'));
+//atatus.notify(new Error('Test Atatus Setup'));
